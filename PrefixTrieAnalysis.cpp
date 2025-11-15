fc14e6d5abde94ecf781d1fbebfc8dcdc68f653f
anya andrews <2019120080@ub.edu.bz>
2025-11-14 20:29:36 -0600

    Added remainder of class to complete insert/search function

--- PrefixTrieAnalysis.cpp
+++ PrefixTrieAnalysis.cpp
@@ -33,7 +33,70 @@ private:
         }
 
     }
-    };
+    
+   public:
+    TriePSearch() {
+        root = make_unique<TriePrefix>();
+    }
+
+
+    void insert(const string& word) {
+        TriePrefix* curr = root.get();
+        for (char a : word) {
+            if (curr->child.find(a) == curr->child.end()) {
+                curr->child[a] = make_unique<TriePrefix>();
+            }
+            curr = curr->child[a].get(); 
+        }
+        curr->EndOfWord = true;
+    }
+
+
+    bool search(const string& word) const {
+        TriePrefix* curr = root.get();
+        for (char b : word) {
+            if (curr->child.find(b) == curr->child.end()) {
+                return false;
+            }
+            curr = curr->child.at(b).get();
+        }
+        return curr != nullptr && curr->EndOfWord;
+    }
+
+
+    bool beginsWith(const string& prefix) const {
+        TriePrefix* curr = root.get();
+        for (char c : prefix) {
+            if (curr->child.find(c) == curr->child.end()) {
+                return false;
+            }
+            curr = curr->child.at(c).get();
+        }
+        return true;
+    }
+
+
+
+
+    vector<string> getWordsWithPrefix(const string& prefix) const {
+        vector<string> result;
+        TriePrefix* curr = root.get();
+
+
+        for (char c : prefix) {
+            if (curr->child.find(c) == curr->child.end()) {
+                return result;
+            }
+            curr = curr->child.at(c).get();
+        }
+
+
+        findAllWords(curr, prefix, result);
+       
+        return result;
+    }
+};
+
 
 int main() {
 
