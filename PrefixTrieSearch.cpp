4ac9cb1dc5853cce10e1db7a8ae1626c1a18ff5c
anya andrews <2019120080@ub.edu.bz>
2025-11-14 19:28:42 -0600

    Implement Insertion 1

--- PrefixTrieSearch.cpp
+++ PrefixTrieSearch.cpp
@@ -6,12 +6,19 @@ using namespace std;
 
 struct TriePrefix {
     map<char, unique_ptr<TriePrefix>> child;
-
-
     bool EndOfWord;
-
-
     TriePrefix() : EndOfWord(false) {}
+};
+
+class TriePSearch {
+private:
+    unique_ptr<TriePrefix> root;
+
+
+public:
+    TriePSearch() {
+        root = make_unique<TriePrefix>();
+    }
 };
 
 int main() {
