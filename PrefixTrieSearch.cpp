27064371322416189dc97837e3f23f13306f6ea8
anya andrews <2019120080@ub.edu.bz>
2025-11-14 19:41:16 -0600

    Searching for word/ prefix

--- PrefixTrieSearch.cpp
+++ PrefixTrieSearch.cpp
@@ -31,6 +31,40 @@ public:
         curr->EndOfWord = true;
     }
 
+     void insert(const string& word) {
+        TriePrefix* curr = root.get();
+        for (char a : word) {
+            if (curr->child.find(a) == curr->child.end()) {
+                curr->child[a] = make_unique<TriePrefix>();
+            }
+            curr = curr->child[a].get();
+        }
+        curr->EndOfWord = true;
+    }
+
+    bool search(const string& word) const {
+        TriePrefix* curr = root.get();
+        for (char b : word) {
+            if (curr->child.find(b) == curr->child.end()) {
+                return false;
+            }
+            curr = curr->child.at(b).get();
+        }
+        return curr != nullptr && curr->EndOfWord;
+    }
+
+
+    bool beginsWith(const string& prefix) const {
+        TriePrefix* curr = root.get();
+        for (char c : prefix) {
+            if (curr->child.find(c) == curr->child.end()) {
+                return false;
+            }
+            curr = curr->child.at(c).get();
+        }
+        return true;
+    }
+
 };
 
 int main() {
