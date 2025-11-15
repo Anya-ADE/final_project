15436667998ffd5d93c165a1778d0bc4a09f59a1
anya andrews <2019120080@ub.edu.bz>
2025-11-14 20:23:08 -0600

    Added Part of Class to begin word search

--- PrefixTrieAnalysis.cpp
+++ PrefixTrieAnalysis.cpp
@@ -8,12 +8,32 @@ using namespace std;
 struct TriePrefix {
     map<char, unique_ptr<TriePrefix>> child;
     bool EndOfWord;
-
-
     TriePrefix() : EndOfWord(false) {}
    
 };
 
+class TriePSearch {
+private:
+    unique_ptr<TriePrefix> root;
+
+
+    void findAllWords(TriePrefix* node, string currentWord, vector<string>& result) const {
+        if (node == nullptr) {
+            return;
+        }
+
+
+        if (node->EndOfWord) {
+            result.push_back(currentWord);
+        }
+
+
+        for (auto const& [character, childPtr] : node->child) {
+            findAllWords(childPtr.get(), currentWord + character, result);
+        }
+
+    }
+    };
 
 int main() {
 
